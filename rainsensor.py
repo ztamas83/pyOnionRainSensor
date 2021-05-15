37aec4f7ec7d2d651593bdec1dbe2a1d3cefa25b
Tamas <ztamas@gmail.com>
2021-05-15 13:46:30 +0200

    first test

--- rainsensor.py
+++ rainsensor.py
@@ -1,5 +1,11 @@
 import gpio
+import time
 
 if __name__ == "__main__":
-    gpio.setup(0, gpio.IN)
-    print(f'GPIO 0 state: {gpio.read(0)}')
+    try:
+        while True:
+            gpio.setup(0, gpio.IN)
+            print(f'GPIO 0 state: {gpio.read(0)}')
+            time.sleep(.5)
+    except KeyboardInterrupt:
+        pass
