3f6893e5a13d570a126cee1a506b1ca973b46996
Tamas <ztamas@gmail.com>
2021-05-15 13:46:30 +0200

    first test

--- rainsensor.py
+++ rainsensor.py
@@ -1,5 +1,12 @@
 import gpio
+import time
+import paho.mqtt.client as mqtt
 
 if __name__ == "__main__":
-    gpio.setup(0, gpio.IN)
-    print(f'GPIO 0 state: {gpio.read(0)}')
+    try:
+        gpio.setup(0, gpio.IN)
+        while True:
+            print(f'GPIO 0 state: {gpio.read(0)}')
+            time.sleep(.5)
+    except KeyboardInterrupt:
+        pass
